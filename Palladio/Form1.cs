c7bc8c1c0e8a61431ff08fa6e54bc1178d6d95f1
keith_i1g3gw <keith_i1g3gw@JOANOFARC>
2016-11-29 19:15:21 -0700

    Add project files.

---
+++ Palladio/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palladio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            room aroom = new room { };
+
+            aroom.DrawRoom(panel1,1, 1, 2, 2, 10, 10, 20, 20);
+
+            
+        
+    }
+    }
+}
